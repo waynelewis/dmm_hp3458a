1be0d7034d805f845905456655bf2ead17331eee
Michael Davidsaver <mdavidsaver@gmail.com>
2019-04-25 14:50:29 -0700

    working

--- iocBoot/ioctest/st.cmd
+++ iocBoot/ioctest/st.cmd
@@ -6,12 +6,14 @@ dmm_registerRecordDeviceDriver pdbbase
 epicsEnvSet("STREAM_PROTOCOL_PATH", "${PWD}/../../protocol")
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "${PWD}/../../db")
 
-vxi11Configure("DMM", "192.168.1.40", 0, "5.0", "gpib0,22")
+vxi11Configure("DMM19", "192.168.1.40", 0, "5.0", "gpib0,19")
+vxi11Configure("DMM22", "192.168.1.40", 0, "5.0", "gpib0,22")
 
-#asynSetTraceMask("DMM",-1,0x3f)
-#asynSetTraceIOMask("DMM",-1,2)
+#asynSetTraceMask("DMM19",-1,0x3f)
+#asynSetTraceIOMask("DMM19",-1,2)
 
-dbLoadRecords("hp3458a.db","P=DMM:,PORT=DMM")
+dbLoadRecords("hp3458a.db","P=DMM:19:,PORT=DMM19")
+dbLoadRecords("hp3458a.db","P=DMM:22:,PORT=DMM22")
 
 iocInit
 
